0715efe35a3beca8311cc3bbdd76865dc94e0f10
RhuamG <rhuamgomes483@gmail.com>
2024-10-12 13:48:18 -0300

    Alteração na tela de pedido; variaveis e nome da janela para deixar coerente com a funcao da tela

--- tela_pedido.py
+++ tela_pedido.py
@@ -1,10 +1,10 @@
 import tkinter as tk
 
 def abrir_janela():
-    nova_janela = tk.Toplevel()  
-    nova_janela.title("Nova Janela")
+    janela_pedido = tk.Toplevel()  
+    janela_pedido.title("Pedido")
     
-    label = tk.Label(nova_janela, text="Bem-vindo à Tela de pedido")
+    label = tk.Label(janela_pedido, text="Bem-vindo à Tela de pedido")
     label.pack(pady=20)
     
-    nova_janela.geometry("400x400")
+    janela_pedido.geometry("400x400")

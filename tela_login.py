a8756b4a72fabc21cd3f1d70d1fb15144aaebb9f
RhuamG <rhuamgomes483@gmail.com>
2024-10-13 17:43:58 -0300

    Criando um Frame para Tela De Login e colocando os elementos dentro dele

--- tela_login.py
+++ tela_login.py
@@ -20,10 +20,10 @@ def abrir_login(janela_incial):
         else:
             messagebox.showerror("Erro", "Usuário ou senha incorretos!")
 
-    #Configurando e Estilizando Janela de Login
+ #Configurando e Estilizando Janela de Login
  janela = tk.Toplevel(janela_incial)
  janela.title("Login")
- janela.geometry("300x200")
+ janela.geometry("300x350")
  janela.resizable(True, True)
  janela.maxsize(width= 350, height = 250)
  janela.minsize(width = 200, height = 200)
@@ -31,18 +31,22 @@ def abrir_login(janela_incial):
  janela.iconbitmap("PIZZARIA\\img\\icon.ico")
 
 
- label_login = tk.Label(janela, text="LOGIN", bd=2, bg = '#d72e4c' , fg = 'white' 
+ janela_frame=tk.Frame(janela, bd = 3, bg = '#c95b6e',
+                     highlightbackground= '#c52f49', highlightthickness= 3 )
+ janela_frame.place(relx=0.02, rely=0.02, relwidth= 0.96, relheight = 0.96)
+
+ label_login = tk.Label(janela_frame, text="LOGIN", bd=2, bg = '#d72e4c' , fg = 'white' 
                             , font = ('verdana', 8, 'bold'))
  label_login.pack(padx=10, pady=10)
 
- campo_login = tk.Entry(janela)
+ campo_login = tk.Entry(janela_frame)
  campo_login.pack(padx=10, pady=10)
 
- campo_senha = tk.Entry(janela, show="*")
+ campo_senha = tk.Entry(janela_frame, show="*")
  campo_senha.pack(padx=10)
 
-    #Chamando uma função na propria pg que esconde a tela atual e executa a função para abrir a outra janela de pedidos
- botao_entrar = tk.Button(janela, text="Entrar", bd=2, bg = '#d72e4c' , fg = 'white' 
+   
+ botao_entrar = tk.Button(janela_frame, text="Entrar", bd=2, bg = '#d72e4c' , fg = 'white' 
                             , font = ('verdana', 8, 'bold') , command=janela_pedido)
  botao_entrar.pack(padx=10, pady=10)
  
